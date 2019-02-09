64c168e79f576d9fa7d31e9ac7e64bfa4e08f51e
Novi Wulandari <novi.wulandari99@gmail.com>
2019-02-09 13:56:39 +0700

    Membuat Shape.java

---
+++ Tugas-2/src/ac/id/stmikamik/Shape.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.id.stmikamik;
+
+/**
+ *
+ * @author Tamu
+ */
+public abstract class Shape {
+    public abstract String getName();
+    public abstract double getArea();
+}
